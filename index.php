437d1e90091130f1b9e18824359365e6545f188e
albertofanini-prog <albertofanini@outlook.com>
2022-03-28 15:31:20 +0200

    Stampa paragrafo e la sua lunghezza  a schermo.

--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
-    $name="Alberto";
+    // $name="Alberto";
     //$name = $_GET['name'];
+
+    $paragrafh = 
+        "
+        Il sesso è una cosa interessante ma non ha un'importanza decisiva.
+        Cioè è meno importante, dal punto di vista fisiologico, della cacata.
+        Un'uomo può tirare avanti per 70 anni senza una donna ma può morire
+        in una settimana se le budella non si muovono.
+        Charles Bukowski.
+        "
 ?>
 
 <!DOCTYPE html>
@@ -12,6 +21,15 @@
     <title>BadWords</title>
 </head>
 <body>
-    <h1>Ciao, sono <?php echo $name; ?>.</h1>
+    <h1><?php echo $paragrafh?></h1>
+    <h4>Questo paragrafo ha: <?php echo strlen($paragrafh)?> caratteri.</h4>
 </body>
 </html>
+
+<!--
+    Creare una variabile con un paragrafo di testo.
+    Stampare a schermo il paragrafo e la sua lunghezza.
+    Una parola da censurare viene passata dall'user tramite il parametro GET.
+    Stampare di nuovo il paragrafo e la sua lunghezza
+        Dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
+-->
